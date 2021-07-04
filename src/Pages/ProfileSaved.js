765e53b4effc1aeda1ebd72fdb3862cab7e45ee8
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-07-04 18:49:28 +0530

    profile page responsive

--- src/Pages/ProfileSaved.js
+++ src/Pages/ProfileSaved.js
@@ -1,49 +1,96 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 //components
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import ProfileInfo from "../Components/Profile/ProfileInfo";
 import ProfileHighlight from "../Components/Profile/ProfileHighlight";
 import ProfileNav from "../Components/Profile/ProfileNav/ProfileNav";
+import ProfileNavM from "../Components/Profile/ProfileNav/ProfileNavM";
 import ProfilePost from "../Components/Profile/ProfilePost";
+import FollowInfo from "../Components/Profile/FollowInfo/FollowInfo";
+import PostModal from "../Components/PostModal/PostModal";
+import NavigaitionBottom from "../Components/NavigationBottom/NavigaitionBottom";
+import ProfileHeader from "../Components/HeaderNav/ProfileHeader";
 //svg
 import savedImgS from "../Images/saved.svg";
 
 const ProfileSaved = () => {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const updateWindowDimensions = () => {
+      setInnerWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", updateWindowDimensions);
+    return () => window.removeEventListener("resize", updateWindowDimensions);
+  }, []);
+
   return (
     <div style={{ overflowX: "hidden" }}>
       <Header />
-      <div className="profile-container" style={{ marginTop: "54px" }}>
-        <ProfileInfo />
-        <div className="highlight-wrapper">
-          <li></li>
-          <ProfileHighlight text="Me" />
-          <ProfileHighlight text="Thoughts" />
-        </div>
-        <ProfileNav
-          imgSaved={savedImgS}
-          textSaved="#262626"
-          borderSaved="1px solid #000"
-          marginSaved="-1px"
-        />
-        <div className="profile-saved-text">
-          <p>Only you can see what you've saved</p>
-        </div>
-        <div className="profile-post-container">
-          <ProfilePost />
-          <ProfilePost className={"profile-post-margin"} />
-          <ProfilePost />
-          <ProfilePost />
-          <ProfilePost className={"profile-post-margin"} />
-          <ProfilePost />
-          <ProfilePost />
-          <ProfilePost className={"profile-post-margin"} />
-          <ProfilePost />
-          <ProfilePost />
-          <ProfilePost className={"profile-post-margin"} />
+      <ProfileHeader innerWidth={innerWidth} />
+      <div className="profile-wrapper">
+        <div
+          className="profile-container"
+          style={{
+            width: innerWidth < 975 ? innerWidth : "975px",
+          }}
+        >
+          <ProfileInfo innerWidth={innerWidth} imgWidth={innerWidth} />
+          <div className="highlight-wrapper">
+            <li></li>
+            <ProfileHighlight text="Me" />
+            <ProfileHighlight text="Thoughts" />
+          </div>
+          <FollowInfo innerWidth={innerWidth} />
+          <ProfileNav
+            imgSaved={savedImgS}
+            textSaved="#262626"
+            borderSaved="1px solid #000"
+            marginSaved="-1px"
+          />
+          <ProfileNavM innerWidth={innerWidth} SelectSaved="#0095f6" />
+          <div
+            className="profile-saved-text"
+            style={{
+              width:
+                innerWidth < 975
+                  ? innerWidth <= 735
+                    ? innerWidth
+                    : innerWidth - 40
+                  : "935px",
+            }}
+          >
+            <p>Only you can see what you've saved</p>
+          </div>
+          <div
+            className="profile-post-container"
+            style={{
+              width:
+                innerWidth < 975
+                  ? innerWidth <= 735
+                    ? innerWidth
+                    : innerWidth - 40
+                  : "935px",
+            }}
+          >
+            <ProfilePost />
+            <ProfilePost className={"profile-post-margin"} />
+            <ProfilePost />
+            <ProfilePost />
+            <ProfilePost className={"profile-post-margin"} />
+            <ProfilePost />
+            <ProfilePost />
+            <ProfilePost className={"profile-post-margin"} />
+            <ProfilePost />
+            <ProfilePost />
+            <ProfilePost className={"profile-post-margin"} />
+          </div>
         </div>
       </div>
       <Footer />
+      <NavigaitionBottom />
     </div>
   );
 };
--- src/Pages/ProfileTagged.js
+++ src/Pages/ProfileTagged.js
@@ -1,46 +1,110 @@
-import React from "react";
-//components
+import React, { useState, useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
+
+//images
+import taggedImgS from "../Images/tagged.svg";
+//component
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import ProfileInfo from "../Components/Profile/ProfileInfo";
 import ProfileHighlight from "../Components/Profile/ProfileHighlight";
 import ProfileNav from "../Components/Profile/ProfileNav/ProfileNav";
+import ProfileNavM from "../Components/Profile/ProfileNav/ProfileNavM";
 import ProfilePost from "../Components/Profile/ProfilePost";
-//images
-import taggedImgS from "../Images/tagged.svg";
+import FollowInfo from "../Components/Profile/FollowInfo/FollowInfo";
+import PostModal from "../Components/PostModal/PostModal";
+import NavigaitionBottom from "../Components/NavigationBottom/NavigaitionBottom";
+import ProfileHeader from "../Components/HeaderNav/ProfileHeader";
 
 const ProfileTagged = () => {
+  const [showPostModal, setShowPostModal] = useState(false);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  const arr = [2, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 66, 6, 6, 3, 33, 3, 3, 3, 3, 3];
+  var j = 1;
+
+  useEffect(() => {
+    const updateWindowDimensions = () => {
+      setInnerWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", updateWindowDimensions);
+
+    return () => window.removeEventListener("resize", updateWindowDimensions);
+  }, []);
+
   return (
     <div style={{ overflowX: "hidden" }}>
       <Header />
-      <div className="profile-container" style={{ marginTop: "54px" }}>
-        <ProfileInfo />
-        <div className="highlight-wrapper">
-          <li></li>
-          <ProfileHighlight text="Me" />
-          <ProfileHighlight text="Thoughts" />
-        </div>
-        <ProfileNav
-          imgTagged={taggedImgS}
-          textTagged="#262626"
-          borderTagged="1px solid #000"
-          marginTagged="-1px"
-        />
-        <div className="profile-post-container">
-          <ProfilePost />
-          <ProfilePost className={"profile-post-margin"} />
-          <ProfilePost />
-          <ProfilePost />
-          <ProfilePost className={"profile-post-margin"} />
-          <ProfilePost />
-          <ProfilePost />
-          <ProfilePost className={"profile-post-margin"} />
-          <ProfilePost />
-          <ProfilePost />
-          <ProfilePost className={"profile-post-margin"} />
+      <ProfileHeader innerWidth={innerWidth} />
+      <div className="profile-wrapper">
+        <div
+          className="profile-container"
+          style={{
+            width: innerWidth < 975 ? innerWidth : "975px",
+          }}
+        >
+          <ProfileInfo innerWidth={innerWidth} imgWidth={innerWidth} />
+          <div className="highlight-wrapper">
+            <li></li>
+            <ProfileHighlight text="Me" />
+            <ProfileHighlight text="Thoughts" />
+          </div>
+          <FollowInfo innerWidth={innerWidth} />
+          <ProfileNav
+            imgTagged={taggedImgS}
+            textTagged="#262626"
+            borderTagged="1px solid #000"
+            marginTagged="-1px"
+          />
+          <ProfileNavM innerWidth={innerWidth} SelectTagged="#0095f6" />
+          <div
+            className="profile-post-container"
+            style={{
+              width:
+                innerWidth < 975
+                  ? innerWidth <= 735
+                    ? innerWidth
+                    : innerWidth - 40
+                  : "935px",
+            }}
+          >
+            {arr.map((x, i) => {
+              if (j === i) {
+                j = j + 3;
+                return (
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    to={{
+                      pathname: "/p/1",
+                      state: { modal: true },
+                    }}
+                  >
+                    <ProfilePost className={"profile-post-margin"} />
+                  </Link>
+                );
+              } else {
+                return (
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    to={{
+                      pathname: "/p/1",
+                      state: { modal: true },
+                    }}
+                  >
+                    <ProfilePost />
+                  </Link>
+                );
+              }
+            })}
+          </div>
         </div>
       </div>
+
+      <NavigaitionBottom />
       <Footer />
+
+      {showPostModal ? <PostModal /> : <></>}
     </div>
   );
 };
