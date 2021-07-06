cb21cc413cb11c41a55f3226ffb253bb714a7852
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-07-06 15:52:04 +0530

    optionModal implemented

--- src/Components/Home/HomePostCard.js
+++ src/Components/Home/HomePostCard.js
@@ -10,8 +10,10 @@ import shareImg from "../../Images/PostCard/share.svg";
 import savedImg from "../../Images/PostCard/saved.svg";
 import savedImgS from "../../Images/PostCard/savedS.svg";
 import emojiImg from "../../Images/PostCard/emoji.svg";
+//components
+import PostOptionModal from "../GenericComponents/PostOptionModal/PostOptionModal";
 
-const HomePostCard = ({ innerWidth }) => {
+const HomePostCard = ({ innerWidth, setOptionBtn }) => {
   const [like, setLike] = useState("false");
   const [saved, setSaved] = useState("false");
 
@@ -20,95 +22,97 @@ const HomePostCard = ({ innerWidth }) => {
   };
 
   return (
-    <div
-      className="post-card-container"
-      style={{
-        width:
-          innerWidth < 1000
-            ? innerWidth < 600
-              ? innerWidth
-              : "600px"
-            : "614px",
-      }}
-    >
-      <div className="post-card-header">
-        <img src={userImg} alt="user profile" />
-        <div className="post-card-header-innerdiv">
-          <a href="">marvelstudios</a>
-          <button>
-            <img
-              style={{ width: "16px", height: "16px" }}
-              src={optionsImg}
-              alt="option"
-            />
-          </button>
+    <>
+      <div
+        className="post-card-container"
+        style={{
+          width:
+            innerWidth < 1000
+              ? innerWidth < 600
+                ? innerWidth
+                : "600px"
+              : "614px",
+        }}
+      >
+        <div className="post-card-header">
+          <img src={userImg} alt="user profile" />
+          <div className="post-card-header-innerdiv">
+            <a href="">marvelstudios</a>
+            <button onClick={setOptionBtn}>
+              <img
+                style={{ width: "16px", height: "16px" }}
+                src={optionsImg}
+                alt="option"
+              />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="post-card-img">
-        <img src={userImg} alt="post image" />
-      </div>
-      <div className="post-card-icons">
-        <div>
+        <div className="post-card-img">
+          <img src={userImg} alt="post image" />
+        </div>
+        <div className="post-card-icons">
+          <div>
+            <button
+              onClick={() => {
+                setLike(!like);
+              }}
+            >
+              <img src={like ? likeImg : likeImgS} alt="like" />
+            </button>
+            <button>
+              <img src={commentImg} alt="comment" />
+            </button>
+            <button>
+              <img src={shareImg} alt="Share" />
+            </button>
+          </div>
           <button
             onClick={() => {
-              setLike(!like);
+              setSaved(!saved);
             }}
           >
-            <img src={like ? likeImg : likeImgS} alt="like" />
-          </button>
-          <button>
-            <img src={commentImg} alt="comment" />
-          </button>
-          <button>
-            <img src={shareImg} alt="Share" />
+            <img src={saved ? savedImg : savedImgS} alt="save button" />
           </button>
         </div>
-        <button
-          onClick={() => {
-            setSaved(!saved);
-          }}
-        >
-          <img src={saved ? savedImg : savedImgS} alt="save button" />
-        </button>
-      </div>
-      <div className="post-card-like-v">
-        <span>
-          <span>3003389 </span>likes
-        </span>
-      </div>
-      <div className="post-card-comment-sec">
-        <div className="post-card-caption">
-          <span>marvelstudios </span>
-          Prepare to meet your match 👊 Tickets and pre-orders are available now
-          for Marvel Studios' @Black.Widow. Experience it
+        <div className="post-card-like-v">
+          <span>
+            <span>3003389 </span>likes
+          </span>
         </div>
-        <a href="">View all 230 comments</a>
-        <div className="post-recent-comment">
-          <span>tamuna_samadashvili </span>
-          Wonderful 🙌
+        <div className="post-card-comment-sec">
+          <div className="post-card-caption">
+            <span>marvelstudios </span>
+            Prepare to meet your match 👊 Tickets and pre-orders are available
+            now for Marvel Studios' @Black.Widow. Experience it
+          </div>
+          <a href="">View all 230 comments</a>
+          <div className="post-recent-comment">
+            <span>tamuna_samadashvili </span>
+            Wonderful 🙌
+          </div>
+        </div>
+        <div className="post-card-upload-time">59 MINUTES AGO</div>
+        <div className="post-card-add-comment">
+          <form onSubmit={handleSubmit}>
+            <button className="post-card-add-comment-button">
+              <img src={emojiImg} alt="emoji" />
+            </button>
+            <input type="text" placeholder="Add a comment..." />
+            <button
+              style={{
+                color: "#0095f6",
+                fontWeight: "600",
+                background: "none",
+                outline: "none",
+                border: "none",
+              }}
+            >
+              Post
+            </button>
+          </form>
         </div>
       </div>
-      <div className="post-card-upload-time">59 MINUTES AGO</div>
-      <div className="post-card-add-comment">
-        <form onSubmit={handleSubmit}>
-          <button className="post-card-add-comment-button">
-            <img src={emojiImg} alt="emoji" />
-          </button>
-          <input type="text" placeholder="Add a comment..." />
-          <button
-            style={{
-              color: "#0095f6",
-              fontWeight: "600",
-              background: "none",
-              outline: "none",
-              border: "none",
-            }}
-          >
-            Post
-          </button>
-        </form>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 //components
 import Header from "../Components/Header";
 import HomeHeader from "../Components/HeaderNav/HomeHeader";
@@ -7,12 +8,17 @@ import HomeRightside from "../Components/Home/HomeRightside";
 import StoryContainer from "../Components/Home/StoryContainer";
 import HomePostCard from "../Components/Home/HomePostCard";
 import NavigaitionBottom from "../Components/NavigationBottom/NavigaitionBottom";
+import PostOptionModal from "../Components/GenericComponents/PostOptionModal/PostOptionModal";
 //images
 import { ReactComponent as HomeS } from "../Images/home-select.svg";
 import userImg from "../Images/profileimg.jpg";
 
 const Home = () => {
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [option, setOption] = useState(false);
+
+  const HomeRef = useRef("");
+
   const a = [1, 1, 1, 1, 1];
 
   useEffect(() => {
@@ -27,18 +33,36 @@ const Home = () => {
     <div style={{ width: "100%", overflowX: "hidden" }}>
       <Header ImgHome={HomeS} />
       <HomeHeader />
-      <div className="home-wrapper">
+      <div ref={HomeRef} className="home-wrapper">
         <div className="home-left">
           <StoryContainer innerWidth={innerWidth} />
           <div>
             {a.map((i) => {
-              return <HomePostCard innerWidth={innerWidth} />;
+              return (
+                <HomePostCard
+                  innerWidth={innerWidth}
+                  setOptionBtn={() => {
+                    setOption(!option);
+                    disableBodyScroll(HomeRef.current);
+                  }}
+                />
+              );
             })}
           </div>
         </div>
         <HomeRightside />
       </div>
       {/* <Footer /> */}
+      {option ? (
+        <PostOptionModal
+          setCloseModal={() => {
+            setOption(!option);
+            enableBodyScroll(HomeRef.current);
+          }}
+        />
+      ) : (
+        <></>
+      )}
       <NavigaitionBottom ImgHome={HomeS} />
     </div>
   );
