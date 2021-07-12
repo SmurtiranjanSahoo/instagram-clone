83e95e9dffc446d4719d210847308c027ad8a7a8
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-07-12 11:55:28 +0530

    user model

---
+++ backend/models/user.js
@@ -0,0 +1,104 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const { v1: uuidv1 } = require("uuid");
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    bio: {
+      type: String,
+      trim: true,
+      maxlength: 200,
+    },
+    username: {
+      type: String,
+      trim: true,
+      unique: true,
+      maxlength: 20,
+    },
+    encry_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+
+    website: {
+      type: String,
+      trim: true,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+
+    posts: {
+      type: Array,
+      default: [],
+    },
+    saved: {
+      type: Array,
+      default: [],
+    },
+    tagged: {
+      type: Array,
+      default: [],
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    highlights: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function (plainpassword) {
+    return this.securePassword(plainpassword) === this.encry_password;
+  },
+  securePassword: function (plainpassword) {
+    if (!plainpassword) return "";
+
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainpassword)
+        .digest("hex");
+    } catch (error) {
+      return "";
+    }
+  },
+};
+
+module.exports = moongoose.model("User", userSchema);
