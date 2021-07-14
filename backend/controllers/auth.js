2552e1140fa9f49d1ba58df00637e620d2b1c165
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-07-14 16:35:44 +0530

    custom middleware

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -57,3 +57,21 @@ exports.signout = (req, res) => {
     message: "User sign out Successfully",
   });
 };
+
+// protected route
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  userProperty: "auth",
+});
+
+// custom middleware
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+
+  if (!checker) {
+    return res.status(403).json({
+      error: "ACCESS DENIED",
+    });
+  }
+  next();
+};
