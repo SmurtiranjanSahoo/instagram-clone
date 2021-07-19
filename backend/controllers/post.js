ac7106a5f791d6da01dea4b346da59191c7b56b4
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-07-19 17:37:35 +0530

    route createPost

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -16,3 +16,43 @@ exports.getPostById = (req, res, next, id) => {
       next();
     });
 };
+
+exports.createPost = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "problem with image",
+      });
+    }
+
+    //destructure the fields
+    const { caption } = fields;
+
+    let post = new Post(fields);
+
+    //handle file here
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "File size too big!",
+        });
+      }
+      post.photo.data = fs.readFileSync(file.photo.path);
+      post.photo.contentType = file.photo.type;
+    }
+    // console.log(post);
+
+    //save to the DB
+    post.save((err, post) => {
+      if (err) {
+        res.status(400).json({
+          error: "Saving tshirt in DB failed",
+        });
+      }
+      res.json(post);
+    });
+  });
+};
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { getPostById } = require("../controllers/post");
-const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+const { getPostById, createPost } = require("../controllers/post");
+const { isSignedIn, isAuthenticated } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
 //all of params
 router.param("userId", getUserById);
 router.param("postId", getPostById);
+
+//Actual Routes
+router.post("/post/create/:userId", isSignedIn, isAuthenticated, createPost);
 
 module.exports = router;
