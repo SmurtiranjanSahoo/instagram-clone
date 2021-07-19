e130c702b2af63f324643dc82bc9baa133259a52
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-07-19 14:13:43 +0530

    route updateUser

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -17,3 +17,21 @@ exports.getUser = (req, res) => {
   req.profile.encry_password = undefined;
   return res.json(req.profile);
 };
+
+exports.updateUser = (req, res) => {
+  User.findByIdAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true, useFindAndModify: false },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "You are not authorized to update this user",
+        });
+      }
+      user.salt = undefined;
+      user.encry_password = undefined;
+      res.json(user);
+    }
+  );
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserById, getUser } = require("../controllers/user");
+const { getUserById, getUser, updateUser } = require("../controllers/user");
 const { isSignedIn, isAuthenticated } = require("../controllers/auth");
 
 router.param("userId", getUserById);
 
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
 
 module.exports = router;
