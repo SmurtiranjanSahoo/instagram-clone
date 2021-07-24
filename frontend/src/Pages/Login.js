4bc4d84556532d8be80efdc3f613fad83a7e7554
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-07-24 20:11:14 +0530

    login implemented

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { signin, authenticate, isAutheticated } from "../auth/auth";
+//images
 import InstaLogo from "../Images/Instagram-written-logo.svg";
 import FbLogo from "../Images/fb-icon.png";
+//componets
 import DownloadBtn from "../Components/DownloadBtn";
 import Footer from "../Components/Footer";
 
 const Login = () => {
+  const [values, setValues] = useState({
+    email: "test1@gmail.com",
+    password: "test1@gmail.com",
+    error: "",
+    loading: false,
+    didRedirect: false,
+  });
+
+  const { email, password, error, loading, didRedirect } = values;
+  const { user } = isAutheticated();
+
+  const handleChange = (val) => (event) => {
+    setValues({ ...values, error: false, [val]: event.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        authenticate(data, () => {
+          setValues({
+            ...values,
+            didRedirect: true,
+          });
+        });
+      }
+    });
+  };
+  //redirect
+  const performRedirect = () => {
+    if (isAutheticated()) {
+      return <Redirect to="/" />;
+    }
+  };
+
   return (
     <div
       style={{
@@ -17,10 +59,16 @@ const Login = () => {
             <div className="insta-written-logo">
               <img src={InstaLogo} alt="Instagram logo" />
             </div>
-            <form className="login-form">
+            <form onSubmit={onSubmit} className="login-form">
               <div style={{ marginTop: "-12px" }} className="input-div">
                 <span className="blocking-span">
-                  <input type="text" className=" login-input" required />
+                  <input
+                    onChange={handleChange("email")}
+                    value={email}
+                    type="text"
+                    className=" login-input"
+                    required
+                  />
                   <span className="floating-label">
                     Phone number, username, or email
                   </span>
@@ -28,13 +76,17 @@ const Login = () => {
               </div>
               <div className="input-div">
                 <span className="blocking-span">
-                  <input type="password" className=" login-input" required />
+                  <input
+                    onChange={handleChange("password")}
+                    value={password}
+                    type="password"
+                    className=" login-input"
+                    required
+                  />
                   <span className="floating-label">Password</span>
                 </span>
               </div>
-              <button disabled className="login-button">
-                Log In
-              </button>
+              <button className="login-button">Log In</button>
               <div className="login-or-wrapper">
                 <div className="login-or-line"></div>
                 <div className="login-or">OR</div>
@@ -60,21 +112,22 @@ const Login = () => {
           <div className="login-card-2">
             <div className="login-card-2-text">
               Don't have an account?{" "}
-              <a
+              <Link
+                to="/accounts/emailsignup"
                 style={{
                   color: "#0095f6",
                   fontWeight: "600",
                   textDecoration: "none",
                 }}
-                href=""
               >
                 Sign up
-              </a>
+              </Link>
             </div>
           </div>
           <DownloadBtn />
         </div>
       </div>
+      {performRedirect()}
       <Footer />
     </div>
   );
