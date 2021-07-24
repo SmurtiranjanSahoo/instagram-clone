1235e54e80e634c6f625cc9466027e0fd5411584
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-07-24 15:36:48 +0530

    auth isAuthenticated method

--- frontend/src/auth/auth.js
+++ frontend/src/auth/auth.js
@@ -49,3 +49,14 @@ export const signout = (next) => {
       .catch((err) => console.log(err));
   }
 };
+
+export const isAutheticated = () => {
+  if (typeof window === "undefined") {
+    return false;
+  }
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};
