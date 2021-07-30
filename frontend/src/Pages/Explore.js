d3e0d4d24c32813cb5f1da1046af9198d32e08cc
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-07-30 16:09:40 +0530

    Explore page

--- frontend/src/Pages/Explore.js
+++ frontend/src/Pages/Explore.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { getAllPosts } from "../helper/apicalls";
+import { isAutheticated } from "../auth/auth";
+
 //component
 import Header from "../Components/Header";
 import Searchbar from "../Components/HeaderNav/Searchbar/Searchbar";
@@ -13,11 +16,25 @@ import { ReactComponent as SearchS } from "../Images/Header/searchS.svg";
 
 const Explore = () => {
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
-
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
   const arr = [2, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 66, 6, 6, 3, 33, 3, 3, 3, 3, 3];
   var j = 1;
 
+  const loadAllPost = () => {
+    const { user, token } = isAutheticated();
+    getAllPosts(user._id, token).then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setPosts(data);
+      }
+    });
+  };
+
   useEffect(() => {
+    loadAllPost();
+
     const updateWindowDimensions = () => {
       setInnerWidth(window.innerWidth);
     };
@@ -35,23 +52,18 @@ const Explore = () => {
           width: innerWidth < 975 ? innerWidth : "975px",
         }}
       >
-        {arr.map((x, i) => {
+        {posts.map((post, i) => {
           if (j === i) {
             j = j + 3;
             return (
-              <Link to="/p/1">
-                <ProfilePost
-                  // imgUrl="https://source.unsplash.com/random"
-                  className={"profile-post-margin"}
-                />
+              <Link key={i} to={"/p/" + post._id}>
+                <ProfilePost post={post} className={"profile-post-margin"} />
               </Link>
             );
           } else {
             return (
-              <Link to="/p/1">
-                <ProfilePost
-                //  imgUrl="https://source.unsplash.com/random"
-                />
+              <Link key={i} to={"/p/" + post._id}>
+                <ProfilePost post={post} />
               </Link>
             );
           }
