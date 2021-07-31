5dd2cfc11126db0f208389dbe61d800ed1c8e13f
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-07-31 13:45:38 +0530

    get user

--- frontend/src/auth/auth.js
+++ frontend/src/auth/auth.js
@@ -62,3 +62,19 @@ export const isAutheticated = () => {
     return false;
   }
 };
+
+//* USER
+export const getUser = (token, userId) => {
+  return fetch(`${API}/user/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
