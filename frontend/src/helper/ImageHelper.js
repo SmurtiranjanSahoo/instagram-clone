94f5f9095e4bf405cca294d5e240f7f9f3e8d311
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-08-01 15:45:28 +0530

    fix: image object fit

--- frontend/src/helper/ImageHelper.js
+++ frontend/src/helper/ImageHelper.js
@@ -7,13 +7,11 @@ const ImageHelper = ({ post }) => {
     ? `${API}/post/photo/${post._id}`
     : `https://images.pexels.com/photos/7645829/pexels-photo-7645829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`;
   return (
-    <div>
-      <img
-        src={imageurl}
-        alt="photo"
-        style={{ maxHeight: "100%", maxWidth: "100%" }}
-      />
-    </div>
+    <img
+      src={imageurl}
+      alt="photo"
+      style={{ height: "100%", width: "100%", objectFit: "cover" }}
+    />
   );
 };
 
