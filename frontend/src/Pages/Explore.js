d5ffda45addcc65d8047abf93851bedbf3b5bc04
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-08-01 15:48:52 +0530

    feat: loading screen

--- frontend/src/Pages/Explore.js
+++ frontend/src/Pages/Explore.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { getAllPosts } from "../helper/apicalls";
 import { isAutheticated } from "../auth/auth";
 
@@ -13,23 +13,25 @@ import NavigaitionBottom from "../Components/NavigationBottom/NavigaitionBottom"
 //images
 import { ReactComponent as ExploreS } from "../Images/explore-select.svg";
 import { ReactComponent as SearchS } from "../Images/Header/searchS.svg";
+import LoadingGif from "../Images/loading.gif";
 
 const Explore = () => {
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
   const [posts, setPosts] = useState([]);
-  const [error, setError] = useState("");
-  const arr = [2, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 66, 6, 6, 3, 33, 3, 3, 3, 3, 3];
+  const [loading, setLoading] = useState(false);
   var j = 1;
 
-  const loadAllPost = () => {
+  const loadAllPost = async () => {
     const { user, token } = isAutheticated();
-    getAllPosts(user._id, token).then((data) => {
+    setLoading(true);
+    await getAllPosts(user._id, token).then((data) => {
       if (data.error) {
-        setError(data.error);
+        console.log(data.error);
       } else {
         setPosts(data);
       }
     });
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -39,8 +41,33 @@ const Explore = () => {
       setInnerWidth(window.innerWidth);
     };
     window.addEventListener("resize", updateWindowDimensions);
-    return () => window.removeEventListener("resize", updateWindowDimensions);
+    return () => {
+      window.removeEventListener("resize", updateWindowDimensions);
+    };
   }, []);
+
+  if (loading) {
+    return (
+      <div>
+        {innerWidth < 735 ? (
+          <Searchbar innerWidth={innerWidth} />
+        ) : (
+          <Header ImgExplore={ExploreS} />
+        )}
+        <div
+          style={{
+            height: "100vh",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img width="50px" height="50px" src={LoadingGif} alt="loading" />
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -75,4 +102,4 @@ const Explore = () => {
   );
 };
 
-export default Explore;
+export default withRouter(Explore);
