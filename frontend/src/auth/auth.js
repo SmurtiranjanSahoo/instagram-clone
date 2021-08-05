866e830218143c8c4f91a084e889c3e65f2e3204
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-08-05 16:03:16 +0530

    feat: update user

--- frontend/src/auth/auth.js
+++ frontend/src/auth/auth.js
@@ -93,3 +93,19 @@ export const getAllUsers = (token, userId) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const updateUser = (userId, token, user) => {
+  return fetch(`${API}/user/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
