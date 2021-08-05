fdd2df180ec99dfd6c5573f69c3a85169208c8ad
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-08-05 12:37:51 +0530

    feat: update like

--- frontend/src/helper/apicalls.js
+++ frontend/src/helper/apicalls.js
@@ -52,3 +52,33 @@ export const deletePost = (postId, userId, token) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const updatePost = (postId, userId, token, post) => {
+  return fetch(`${API}/post/${postId}/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: post,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const updatePostLikes = (postId, userId, token, post) => {
+  return fetch(`${API}/postlike/${postId}/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: post,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
