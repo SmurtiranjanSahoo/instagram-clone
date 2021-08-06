a70f30c064708a9d28be6d3a8e1d6ae6c3c19a3e
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-08-06 10:15:18 +0530

    feat: add profileid and get users in dynamic routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,17 +39,17 @@ const App = ({ location }) => {
         <PrivateRoute exact path="/" component={Home} />
         <Route exact path="/accounts/login" component={Login} />
         <Route exact path="/accounts/emailsignup" component={Signup} />
-        <PrivateRoute exact path="/profile" component={Profile} />
         <PrivateRoute exact path="/profile/channel" component={ProfileIgtv} />
         <PrivateRoute exact path="/profile/saved" component={ProfileSaved} />
         <PrivateRoute exact path="/profile/tagged" component={ProfileTagged} />
         <PrivateRoute exact path="/direct/inbox" component={DirectInbox} />
         <PrivateRoute exact path="/explore" component={Explore} />
         <PrivateRoute exact path="/stories" component={StoryPlay} />
-        <PrivateRoute exact path="/accounts/activity" component={Activity} />
+        <PrivateRoute exact path="/activity" component={Activity} />
         <PrivateRoute exact path="/explore/search" component={ExploreSearch} />
         <PrivateRoute exact path="/p/comments" component={Comments} />
         <PrivateRoute exact path="/create" component={CreatePost} />
+        <PrivateRoute exact path="/:profileid" component={Profile} />
 
         <Route exact path="/p/:postid">
           <PostModal isModal={isModal} />
