1f0da2b76e06238826464de134f157429aa115d2
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-09-01 15:25:08 +0530

    feat: add UserReducer

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 //reducers
 import PostReducer from "../reducer/PostReducer";
+import UserReducer from "../reducer/UserReducer";
 
 const rootReducer = combineReducers({
   PostReducer,
+  UserReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
