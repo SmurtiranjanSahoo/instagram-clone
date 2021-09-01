670177c7c2ecd59cee29754511d483ce3d1c14f6
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-09-01 15:24:13 +0530

    feat: add getAllUsers

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,87 @@
+import {
+  FETCH_USER_BEGIN,
+  FETCH_USER_SUCCESS,
+  FETCH_USER_FAILURE,
+  FETCH_ALLUSER_BEGIN,
+  FETCH_ALLUSER_SUCCESS,
+  FETCH_ALLUSER_FAILURE,
+  FETCH_USERBYUSERNAME_BEGIN,
+  FETCH_USERBYUSERNAME_SUCCESS,
+  FETCH_USERBYUSERNAME_FAILURE,
+  UPDATEUSER_BEGIN,
+  UPDATEUSER_SUCCESS,
+  UPDATEUSER_FAILURE,
+} from "../actions/constants/action-types";
+import {
+  isAutheticated,
+  getUser,
+  getAllUsers,
+  getUserByUsername,
+  updateUser,
+} from "../auth/auth";
+
+//fetch user
+export const fetchUser = (userId) => {
+  return (dispatch) => {
+    dispatch(fetchUserBegin());
+    const { token } = isAutheticated();
+    getUser(token, userId)
+      .then((data) => {
+        if (data.error) {
+          dispatch(fetchUserFailure(data.error));
+        }
+        dispatch(fetchUserSuccess(data));
+      })
+      .catch((err) => {
+        const errMsg = err.message;
+        dispatch(fetchUserFailure(errMsg));
+      });
+  };
+};
+
+export const fetchUserBegin = () => ({
+  type: FETCH_USER_BEGIN,
+});
+
+export const fetchUserSuccess = (user) => ({
+  type: FETCH_USER_SUCCESS,
+  payload: user,
+});
+
+export const fetchUserFailure = (error) => ({
+  type: FETCH_USER_FAILURE,
+  payload: error,
+});
+
+//fetch all user
+export const fetchAllUser = () => {
+  return (dispatch) => {
+    dispatch(fetchAllUserBegin());
+    const { token, user } = isAutheticated();
+    getAllUsers(token, user._id)
+      .then((data) => {
+        if (data.error) {
+          dispatch(fetchAllUserFailure(data.error));
+        }
+        dispatch(fetchAllUserSuccess(data));
+      })
+      .catch((err) => {
+        const errMsg = err.message;
+        dispatch(fetchAllUserFailure(errMsg));
+      });
+  };
+};
+
+export const fetchAllUserBegin = () => ({
+  type: FETCH_ALLUSER_BEGIN,
+});
+
+export const fetchAllUserSuccess = (alluser) => ({
+  type: FETCH_ALLUSER_SUCCESS,
+  payload: alluser,
+});
+
+export const fetchAllUserFailure = (error) => ({
+  type: FETCH_ALLUSER_FAILURE,
+  payload: error,
+});
---
+++ frontend/src/reducer/UserReducer.js
@@ -0,0 +1,60 @@
+import {
+  FETCH_USER_BEGIN,
+  FETCH_USER_SUCCESS,
+  FETCH_USER_FAILURE,
+  FETCH_ALLUSER_BEGIN,
+  FETCH_ALLUSER_SUCCESS,
+  FETCH_ALLUSER_FAILURE,
+  FETCH_USERBYUSERNAME_BEGIN,
+  FETCH_USERBYUSERNAME_SUCCESS,
+  FETCH_USERBYUSERNAME_FAILURE,
+  UPDATEUSER_BEGIN,
+  UPDATEUSER_SUCCESS,
+  UPDATEUSER_FAILURE,
+} from "../actions/constants/action-types";
+
+const INITIAL_USER_STATE = {
+  isUserLoading: false,
+  isGettingAllUser: false,
+  isUserUpdating: false,
+  userDetails: {},
+  allUsers: [],
+  error: "",
+};
+
+const UserReducer = (state = INITIAL_USER_STATE, action) => {
+  switch (action.type) {
+    case FETCH_USER_BEGIN:
+      return { ...state, isUserLoading: true };
+    case FETCH_USER_SUCCESS:
+      return { ...state, isUserLoading: false, userDetails: action.payload };
+    case FETCH_USER_FAILURE:
+      return { ...state, isUserLoading: false, error: action.payload };
+
+    case FETCH_ALLUSER_BEGIN:
+      return { ...state, isGettingAllUser: true };
+    case FETCH_ALLUSER_SUCCESS:
+      return { ...state, isGettingAllUser: false, allUsers: action.payload };
+    case FETCH_ALLUSER_FAILURE:
+      return { ...state, isGettingAllUser: false, error: action.payload };
+
+    case FETCH_USERBYUSERNAME_BEGIN:
+      return { ...state, isUserLoading: true };
+    case FETCH_USERBYUSERNAME_SUCCESS:
+      return { ...state, isUserLoading: false, userDetails: action.payload };
+    case FETCH_USERBYUSERNAME_FAILURE:
+      return { ...state, isUserLoading: false, error: action.payload };
+
+    case UPDATEUSER_BEGIN:
+      return { ...state, isGettingAllUser: true };
+    case UPDATEUSER_SUCCESS:
+      return { ...state, isGettingAllUser: false };
+    case UPDATEUSER_FAILURE:
+      return { ...state, isGettingAllUser: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export default UserReducer;
