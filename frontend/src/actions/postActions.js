42ec54d795b26be121d2b595655394aabcc70991
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-09-01 12:23:20 +0530

    feat: add fetchAllPost

--- frontend/src/actions/postActions.js
+++ frontend/src/actions/postActions.js
@@ -2,10 +2,14 @@ import {
   CREATEPOST_BEGIN,
   CREATEPOST_SUCCESS,
   CREATEPOST_FAILURE,
+  FETCH_ALLPOST_BEGIN,
+  FETCH_ALLPOST_SUCCESS,
+  FETCH_ALLPOST_FAILURE,
 } from "../actions/constants/action-types";
-import { createPost } from "../helper/apicalls";
+import { createPost, getAllPosts } from "../helper/apicalls";
 import { isAutheticated } from "../auth/auth";
 
+//post create
 export const postCreate = (post) => {
   return (dispatch) => {
     dispatch(postCreateBegin(post));
@@ -35,5 +39,39 @@ export const postCreateSuccess = () => ({
 
 export const postCreateFailure = (error) => ({
   type: CREATEPOST_FAILURE,
+  payload: error,
+});
+
+//get all posts
+export const fetchAllPost = () => {
+  return (dispatch) => {
+    dispatch(fetchAllPostBegin());
+    const { user, token } = isAutheticated();
+    getAllPosts(user._id, token)
+      .then((data) => {
+        if (data.error) {
+          dispatch(fetchAllPostFailure(data.error));
+        }
+        // console.log(data);
+        dispatch(fetchAllPostSuccess(data));
+      })
+      .catch((err) => {
+        const errMsg = err.message;
+        dispatch(fetchAllPostFailure(errMsg));
+      });
+  };
+};
+
+export const fetchAllPostBegin = () => ({
+  type: FETCH_ALLPOST_BEGIN,
+});
+
+export const fetchAllPostSuccess = (allpost) => ({
+  type: FETCH_ALLPOST_SUCCESS,
+  payload: allpost,
+});
+
+export const fetchAllPostFailure = (error) => ({
+  type: FETCH_ALLPOST_FAILURE,
   payload: error,
 });
--- frontend/src/reducer/PostReducer.js
+++ frontend/src/reducer/PostReducer.js
@@ -2,11 +2,15 @@ import {
   CREATEPOST_BEGIN,
   CREATEPOST_SUCCESS,
   CREATEPOST_FAILURE,
+  FETCH_ALLPOST_BEGIN,
+  FETCH_ALLPOST_SUCCESS,
+  FETCH_ALLPOST_FAILURE,
 } from "../actions/constants/action-types";
 
 const INITIAL_POST_STATE = {
   isPostLoading: false,
   isPostCreating: false,
+  isGettingAllPost: false,
   createPostDetails: {},
   postDetails: {},
   allPosts: [],
@@ -25,6 +29,13 @@ const PostReducer = (state = INITIAL_POST_STATE, action) => {
       return { ...state, isPostCreating: false };
     case CREATEPOST_FAILURE:
       return { ...state, isPostCreating: false, error: action.payload };
+
+    case FETCH_ALLPOST_BEGIN:
+      return { ...state, isGettingAllPost: true };
+    case FETCH_ALLPOST_SUCCESS:
+      return { ...state, isGettingAllPost: false, allPosts: action.payload };
+    case FETCH_ALLPOST_FAILURE:
+      return { ...state, isGettingAllPost: false, error: action.payload };
 
     default:
       return state;
