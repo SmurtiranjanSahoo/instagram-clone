47b82444540889588ec5ba0804dc68e9d1842f6d
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-09-03 21:14:45 +0530

    feat: add redux & dynamic data

--- frontend/src/Components/Profile/ProfileInfo.js
+++ frontend/src/Components/Profile/ProfileInfo.js
@@ -1,55 +1,49 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { connect } from "react-redux";
+import {
+  fetchUser,
+  fetchUserByUsername,
+  userUpdate,
+} from "../../actions/userActions";
+import { isAutheticated, updateUser, getUser } from "../../auth/auth";
+//icon
 import SettingIcon from "../../Images/settings.svg";
 import ProfileImg from "../../Images/profileimg.jpg";
-import { isAutheticated, updateUser, getUser } from "../../auth/auth";
-//context
-import { userContext } from "../../Context/userContext";
 
-const ProfileInfo = ({ innerWidth, imgWidth }) => {
-  const { user, token } = isAutheticated();
+const ProfileInfo = ({
+  innerWidth,
+  imgWidth,
+  userState,
+  fetchUser,
+  fetchUserByUsername,
+  userUpdate,
+}) => {
+  const { profileid } = useParams();
+  const { user } = isAutheticated();
+  const { userDetails, userUsernameDetails } = userState;
 
-  const UserContext = useContext(userContext);
-  // console.log(UserContext);
-  const { name, followers, followings, posts, username, _id } =
-    UserContext.currentUser;
   const [follow, setFollow] = useState(false);
   const [updateInfo, setUpdateInfo] = useState({
-    followings: _id,
+    followings: userUsernameDetails._id,
   });
-  useEffect(() => {
-    // let user = JSON.parse(localStorage.getItem("jwt"));
-    getUser(token, user._id).then((data) => {
-      if (data.error) {
-        console.log(data.error);
-      } else {
-        setFollow(data.followings?.includes(_id));
-      }
-    });
-  }, []);
 
   const updateFollow = () => {
     if (!follow === true) {
       setFollow(true);
-      updateUser(user._id, token, updateInfo).then((data) => {
-        if (data.error) {
-          console.log(data.error);
-        } else {
-          console.log(data);
-        }
-      });
+      userUpdate(updateInfo);
       //Todo Add user to followers
     } else {
       setFollow(false);
-      updateUser(user._id, token, updateInfo).then((data) => {
-        if (data.error) {
-          console.log(data.error);
-        } else {
-          console.log(data);
-        }
-      });
+      userUpdate(updateInfo);
     }
   };
+
+  useEffect(() => {
+    fetchUserByUsername({ username: profileid });
+    fetchUser(user._id);
+    setFollow(userDetails.followings?.includes(userUsernameDetails._id));
+  }, [profileid, userDetails.followings]);
 
   return (
     <div>
@@ -92,12 +86,14 @@ const ProfileInfo = ({ innerWidth, imgWidth }) => {
                 whiteSpace: "nowrap",
               }}
             >
-              {username}
+              {user.username === profileid
+                ? userDetails.username
+                : userUsernameDetails.username}
             </h2>
-            {user._id === _id ? (
+            {user._id === userUsernameDetails._id ? (
               <span>
                 <Link
-                  to="/"
+                  to="/edit"
                   style={{ textDecoration: "none", color: "#262626" }}
                 >
                   Edit Profile
@@ -122,19 +118,36 @@ const ProfileInfo = ({ innerWidth, imgWidth }) => {
           </div>
           <ul className="profile-info-pff">
             <span style={{ marginRight: "40px" }}>
-              <span style={{ fontWeight: "600" }}>{posts?.length}</span> posts
+              <span style={{ fontWeight: "600" }}>
+                {user.username === profileid
+                  ? userDetails.posts?.length
+                  : userUsernameDetails.posts?.length}
+              </span>{" "}
+              posts
             </span>
             <span style={{ marginRight: "40px" }}>
-              <span style={{ fontWeight: "600" }}>{followers?.length}</span>{" "}
+              <span style={{ fontWeight: "600" }}>
+                {user.username === profileid
+                  ? userDetails.followers?.length
+                  : userUsernameDetails.followers?.length}
+              </span>{" "}
               followers
             </span>
             <span>
-              <span style={{ fontWeight: "600" }}>{followings?.length}</span>{" "}
+              <span style={{ fontWeight: "600" }}>
+                {user.username === profileid
+                  ? userDetails.followings?.length
+                  : userUsernameDetails.followings?.length}
+              </span>{" "}
               following
             </span>
           </ul>
           <div className="profile-name">
-            <h1>{name}</h1>
+            <h1>
+              {user.username === profileid
+                ? userDetails.name
+                : userUsernameDetails.name}
+            </h1>
             <span>Video Creator</span>
             <div className="profile-bio"></div>
             <a href="">youtube.com/trtechlesson</a>
@@ -142,7 +155,12 @@ const ProfileInfo = ({ innerWidth, imgWidth }) => {
         </section>
       </div>
       <div className="profile-name-m">
-        <h1>{name}</h1>
+        <h1>
+          {" "}
+          {user.username === profileid
+            ? userDetails.name
+            : userUsernameDetails.name}
+        </h1>
         <span>Video Creator</span>
         <div className="profile-bio"></div>
         <a href="">youtube.com/trtechlesson</a>
@@ -151,4 +169,14 @@ const ProfileInfo = ({ innerWidth, imgWidth }) => {
   );
 };
 
-export default ProfileInfo;
+const mapStateToProps = (state) => ({
+  userState: state.UserReducer,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchUserByUsername: (username) => dispatch(fetchUserByUsername(username)),
+  fetchUser: (id) => dispatch(fetchUser(id)),
+  userUpdate: (user) => dispatch(userUpdate(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileInfo);
