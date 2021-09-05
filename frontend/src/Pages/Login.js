e9b244e1ff753eafd3233c91f97196e2de89d7a8
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-09-05 21:01:45 +0530

    feat: mobile responsive

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -49,83 +49,82 @@ const Login = () => {
 
   return (
     <div
+      className="signup-signin-wrapper"
       style={{
         height: "100vh",
       }}
     >
       <div className="login-wrapper-main">
-        <div>
-          <div className="login-card-1">
-            <div className="insta-written-logo">
-              <img src={InstaLogo} alt="Instagram logo" />
-            </div>
-            <form onSubmit={onSubmit} className="login-form">
-              <div style={{ marginTop: "-12px" }} className="input-div">
-                <span className="blocking-span">
-                  <input
-                    onChange={handleChange("email")}
-                    value={email}
-                    type="text"
-                    className=" login-input"
-                    required
-                  />
-                  <span className="floating-label">
-                    Phone number, username, or email
-                  </span>
-                </span>
-              </div>
-              <div className="input-div">
-                <span className="blocking-span">
-                  <input
-                    onChange={handleChange("password")}
-                    value={password}
-                    type="password"
-                    className=" login-input"
-                    required
-                  />
-                  <span className="floating-label">Password</span>
-                </span>
-              </div>
-              <button className="login-button">Log In</button>
-              <div className="login-or-wrapper">
-                <div className="login-or-line"></div>
-                <div className="login-or">OR</div>
-                <div className="login-or-line"></div>
-              </div>
-              <div className="login-fb">
-                <img
-                  style={{
-                    height: "15px",
-                    width: "15px",
-                    alignSelf: "center",
-                    margin: "0 8px",
-                    borderRadius: "1px",
-                  }}
-                  src={FbLogo}
-                  alt="fb icon"
+        <div className="login-card-1">
+          <div className="insta-written-logo">
+            <img src={InstaLogo} alt="Instagram logo" />
+          </div>
+          <form onSubmit={onSubmit} className="login-form">
+            <div style={{ marginTop: "-12px" }} className="input-div">
+              <span className="blocking-span">
+                <input
+                  onChange={handleChange("email")}
+                  value={email}
+                  type="text"
+                  className=" login-input"
+                  required
                 />
-                <p className="login-fb-text"> Log in with Facebook</p>
-              </div>
-              <div className="login-forget-pass">Forgot password?</div>
-            </form>
-          </div>
-          <div className="login-card-2">
-            <div className="login-card-2-text">
-              Don't have an account?{" "}
-              <Link
-                to="/accounts/emailsignup"
-                style={{
-                  color: "#0095f6",
-                  fontWeight: "600",
-                  textDecoration: "none",
-                }}
-              >
-                Sign up
-              </Link>
+                <span className="floating-label">
+                  Phone number, username, or email
+                </span>
+              </span>
             </div>
-          </div>
-          <DownloadBtn />
+            <div className="input-div">
+              <span className="blocking-span">
+                <input
+                  onChange={handleChange("password")}
+                  value={password}
+                  type="password"
+                  className=" login-input"
+                  required
+                />
+                <span className="floating-label">Password</span>
+              </span>
+            </div>
+            <button className="login-button">Log In</button>
+            <div className="login-or-wrapper">
+              <div className="login-or-line"></div>
+              <div className="login-or">OR</div>
+              <div className="login-or-line"></div>
+            </div>
+            <div className="login-fb">
+              <img
+                style={{
+                  height: "15px",
+                  width: "15px",
+                  alignSelf: "center",
+                  margin: "0 8px",
+                  borderRadius: "1px",
+                }}
+                src={FbLogo}
+                alt="fb icon"
+              />
+              <p className="login-fb-text"> Log in with Facebook</p>
+            </div>
+            <div className="login-forget-pass">Forgot password?</div>
+          </form>
         </div>
+        <div className="login-card-2">
+          <div className="login-card-2-text">
+            Don't have an account?{" "}
+            <Link
+              to="/accounts/emailsignup"
+              style={{
+                color: "#0095f6",
+                fontWeight: "600",
+                textDecoration: "none",
+              }}
+            >
+              Sign up
+            </Link>
+          </div>
+        </div>
+        <DownloadBtn />
       </div>
       {performRedirect()}
       <Footer />
--- frontend/src/Pages/Signup.js
+++ frontend/src/Pages/Signup.js
@@ -11,10 +11,10 @@ import Footer from "../Components/Footer";
 
 const Signup = () => {
   const [values, setValues] = useState({
-    name: "test1",
-    email: "test1@gmail.com",
-    username: "test1",
-    password: "test1@gmail.com",
+    name: "",
+    email: "",
+    username: "",
+    password: "",
     error: "",
     success: false,
   });
@@ -47,116 +47,115 @@ const Signup = () => {
 
   return (
     <div
+      className="signup-signin-wrapper"
       style={{
         height: "100vh",
       }}
     >
       <div className="login-wrapper-main">
-        <div>
-          <div className="signup-card-1">
-            <div className="insta-written-logo">
-              <img src={InstaLogo} alt="Instagram logo" />
-            </div>
-            <div className="signup-text1">
-              Sign up to see photos and videos from your friends.
-            </div>
-            <div className="signup-fb">
-              <img
-                style={{
-                  height: "16px",
-                  width: "16px",
-                  alignSelf: "center",
-                  margin: "0 8px",
-                  borderRadius: "1px",
-                }}
-                src={FbLogo}
-                alt="fb icon"
-              />
-              <p className="signup-fb-text"> Log in with Facebook</p>
-            </div>
-            <div
-              style={{ marginTop: "5px", marginBottom: "-4px" }}
-              className="login-or-wrapper"
-            >
-              <div className="login-or-line"></div>
-              <div className="login-or">OR</div>
-              <div className="login-or-line"></div>
-            </div>
-            <form onSubmit={onSubmit} className="login-form">
-              <div className="input-div">
-                <span className="blocking-span">
-                  <input
-                    value={email}
-                    onChange={handleChange("email")}
-                    type="email"
-                    className=" login-input"
-                    required
-                  />
-                  <span className="floating-label">Mobile Number or Email</span>
-                </span>
-              </div>
-              <div className="input-div">
-                <span className="blocking-span">
-                  <input
-                    value={name}
-                    onChange={handleChange("name")}
-                    type="text"
-                    className=" login-input"
-                    required
-                  />
-                  <span className="floating-label">Full Name</span>
-                </span>
-              </div>
-              <div className="input-div">
-                <span className="blocking-span">
-                  <input
-                    value={username}
-                    onChange={handleChange("username")}
-                    type="text"
-                    className=" login-input"
-                    required
-                  />
-                  <span className="floating-label">Username</span>
-                </span>
-              </div>
-              <div className="input-div">
-                <span className="blocking-span">
-                  <input
-                    value={password}
-                    onChange={handleChange("password")}
-                    type="password"
-                    className=" login-input"
-                    required
-                  />
-                  <span className="floating-label">Password</span>
-                </span>
-              </div>
-              <button className="login-button">Sign up</button>
-            </form>
-            <div className="signup-tc">
-              By signing up, you agree to our{" "}
-              <span className="signup-tc-bold"> Terms</span> ,
-              <span className="signup-tc-bold"> Data Policy</span> and{" "}
-              <span className="signup-tc-bold"> Cookies Policy</span> .
-            </div>
+        <div className="signup-card-1">
+          <div className="insta-written-logo">
+            <img src={InstaLogo} alt="Instagram logo" />
           </div>
-          <div className="login-card-2">
-            <div className="login-card-2-text">
-              Have an account?{" "}
-              <Link
-                to="/accounts/login"
-                style={{
-                  color: "#0095f6",
-                  fontWeight: "600",
-                  textDecoration: "none",
-                }}
-              >
-                Log in
-              </Link>
-            </div>
+          <div className="signup-text1">
+            Sign up to see photos and videos from your friends.
           </div>
-          <DownloadBtn />
+          <div className="signup-fb">
+            <img
+              style={{
+                height: "16px",
+                width: "16px",
+                alignSelf: "center",
+                margin: "0 8px",
+                borderRadius: "1px",
+              }}
+              src={FbLogo}
+              alt="fb icon"
+            />
+            <p className="signup-fb-text"> Log in with Facebook</p>
+          </div>
+          <div
+            style={{ marginTop: "5px", marginBottom: "-4px" }}
+            className="login-or-wrapper"
+          >
+            <div className="login-or-line"></div>
+            <div className="login-or">OR</div>
+            <div className="login-or-line"></div>
+          </div>
+          <form onSubmit={onSubmit} className="login-form">
+            <div className="input-div">
+              <span className="blocking-span">
+                <input
+                  value={email}
+                  onChange={handleChange("email")}
+                  type="email"
+                  className=" login-input"
+                  required
+                />
+                <span className="floating-label">Mobile Number or Email</span>
+              </span>
+            </div>
+            <div className="input-div">
+              <span className="blocking-span">
+                <input
+                  value={name}
+                  onChange={handleChange("name")}
+                  type="text"
+                  className=" login-input"
+                  required
+                />
+                <span className="floating-label">Full Name</span>
+              </span>
+            </div>
+            <div className="input-div">
+              <span className="blocking-span">
+                <input
+                  value={username}
+                  onChange={handleChange("username")}
+                  type="text"
+                  className=" login-input"
+                  required
+                />
+                <span className="floating-label">Username</span>
+              </span>
+            </div>
+            <div className="input-div">
+              <span className="blocking-span">
+                <input
+                  value={password}
+                  onChange={handleChange("password")}
+                  type="password"
+                  className=" login-input"
+                  required
+                />
+                <span className="floating-label">Password</span>
+              </span>
+            </div>
+            <button className="login-button">Sign up</button>
+          </form>
+          <div className="signup-tc">
+            By signing up, you agree to our{" "}
+            <span className="signup-tc-bold"> Terms</span> ,
+            <span className="signup-tc-bold"> Data Policy</span> and{" "}
+            <span className="signup-tc-bold"> Cookies Policy</span> .
+          </div>
         </div>
+        <div className="login-card-2">
+          <div className="login-card-2-text">
+            Have an account?{" "}
+            <Link
+              to="/accounts/login"
+              style={{
+                color: "#0095f6",
+                fontWeight: "600",
+                textDecoration: "none",
+              }}
+            >
+              Log in
+            </Link>
+          </div>
+        </div>
+        <DownloadBtn />
       </div>
       <Footer />
     </div>
