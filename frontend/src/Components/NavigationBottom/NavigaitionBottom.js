eb66ed1858b592088e98cd1c38502d20b969b551
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-09-06 20:15:31 +0530

    reactor: user images

--- frontend/src/Components/NavigationBottom/NavigaitionBottom.js
+++ frontend/src/Components/NavigationBottom/NavigaitionBottom.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./NavigaitionBottom.css";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchUser } from "../../actions/userActions";
 import { isAutheticated } from "../../auth/auth";
 //images
 import { ReactComponent as Home } from "../../Images/home.svg";
@@ -8,17 +10,23 @@ import { ReactComponent as Like } from "../../Images/like.svg";
 import { ReactComponent as Search } from "../../Images/Header/search.svg";
 import { ReactComponent as NewPost } from "../../Images/Header/newPost.svg";
 import userImg from "../../Images/profileimg.jpg";
+//components
+import UserPhotoHelper from "../../helper/UserPhotoHelper";
 
 const NavigaitionBottom = ({
   ImgHome = Home,
   ImgLike = Like,
   ImgSearch = Search,
+  userState,
+  fetchUser,
 }) => {
   const { user } = isAutheticated();
+  const { userDetails } = userState;
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
   const [photo, setPhoto] = useState("");
 
   useEffect(() => {
+    fetchUser(user._id);
     const updateWindowDimensions = () => {
       setInnerWidth(window.innerWidth);
     };
@@ -60,14 +68,29 @@ const NavigaitionBottom = ({
         style={{ width: innerWidth / 5 }}
         className="navigation-img header-profile-img"
       >
-        <img
-          className="header-icon-container-profile"
-          src={userImg}
-          alt="User image"
-        />
+        {userDetails?.photo ? (
+          <UserPhotoHelper
+            className="header-icon-container-profile"
+            user={userDetails}
+          />
+        ) : (
+          <img
+            className="header-icon-container-profile"
+            src={userImg}
+            alt="User image"
+          />
+        )}
       </Link>
     </div>
   );
 };
 
-export default NavigaitionBottom;
+const mapStateToProps = (state) => ({
+  userState: state.UserReducer,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchUser: (id) => dispatch(fetchUser(id)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavigaitionBottom);
