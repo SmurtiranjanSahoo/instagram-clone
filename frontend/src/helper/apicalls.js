2188e9beafbcac34d9a2baa27d9f41c8353e7bf7
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-09-07 22:19:56 +0530

    feat: story helper methods

--- frontend/src/helper/apicalls.js
+++ frontend/src/helper/apicalls.js
@@ -1,11 +1,11 @@
 import { API } from "../backend";
 
+//Story
 export const createPost = (userId, token, post) => {
   return fetch(`${API}/post/create/${userId}`, {
     method: "POST",
     headers: {
       Accept: "application/json",
-      //   "Content-Type": "multipart/form-data",
       Authorization: `Bearer ${token}`,
     },
     body: post,
@@ -76,6 +76,59 @@ export const updatePostLikeNComment = (postId, userId, token, post) => {
       Authorization: `Bearer ${token}`,
     },
     body: post,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+//story
+export const createStory = (userId, token, story) => {
+  return fetch(`${API}/story/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: story,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const getAllStories = (userId, token) => {
+  return fetch(`${API}/stories/${userId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const getStory = (storyId) => {
+  return fetch(`${API}/story/${storyId}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const deleteStory = (storyId, userId, token) => {
+  return fetch(`${API}/story/${storyId}/${userId}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   })
     .then((response) => {
       return response.json();
