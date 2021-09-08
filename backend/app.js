9686490e26aef801e4d892ad07dcbac54a02a344
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-09-08 18:41:16 +0530

    refactor: cors

--- backend/app.js
+++ backend/app.js
@@ -29,9 +29,10 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 const corsConfig = {
-  origin: "*",
+  origin: ["https://instagram-tr.vercel.app", "http://localhost:3000"],
   credential: true,
   optionsSuccessStatus: 200,
+  allowedHeaders: ["Content-Type"],
 };
 app.use(cors(corsConfig));
 app.options("*", cors(corsConfig));
