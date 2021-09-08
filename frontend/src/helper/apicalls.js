e42bc289d11e20fbb5c4b1fbdc192e33fe01b26a
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-09-08 14:38:03 +0530

    refactor: add headers

--- frontend/src/helper/apicalls.js
+++ frontend/src/helper/apicalls.js
@@ -24,6 +24,7 @@ export const getAllPosts = (userId, token) => {
       Authorization: `Bearer ${token}`,
       Accept: "application/json",
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
     },
   })
     .then((response) => {
