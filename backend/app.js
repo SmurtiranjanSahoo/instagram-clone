7e0f087aaf09b70e3b18273cb2988b08590483d0
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-09-11 12:20:26 +0530

    refactor: cors error

--- backend/app.js
+++ backend/app.js
@@ -37,13 +37,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   next();
 });
 
